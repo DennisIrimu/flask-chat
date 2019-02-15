5a83e30d31a81392f7d51b447cdd8299f0476d4b
DennisIrimu <lyricaldnyt@gmail.com>
2019-02-15 16:20:07 +0300

    Created first view

--- Desktop/chat/main.py
+++ Desktop/chat/main.py
@@ -5,5 +5,12 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'illenium'
 socketio = SocketIO(app)
 
+@app.route('/')
+def sessions():
+    return render_template('session.html')
+
+def messageReceived(methods=['GET', 'POST']):
+    print('BLUE TICKS...TING!!!')
+
 if __name__ == '__main__'
     socket.run(app, debug=True)
