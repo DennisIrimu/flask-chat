eb19ffcd5f593c827ffd458e7a985b9d68b47480
DennisIrimu <lyricaldnyt@gmail.com>
2019-02-15 16:16:44 +0300

    Created run method and socketio variable to run the app

--- Desktop/chat/main.py
+++ Desktop/chat/main.py
@@ -1,2 +1,9 @@
 from flask import Flask
-from flask_socketio import SocketIO 
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'illenium'
+socketio = SocketIO(app)
+
+if __name__ == '__main__'
+    socket.run(app, debug=True)
